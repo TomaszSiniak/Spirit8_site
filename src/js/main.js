f173fe876fbea386de43085614240204478ae140
TomaszSiniak <siniak.tomasz@gmail.com>
2017-09-26 07:04:52 +0200

    added section - team

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,47 @@ $(document).ready(function() {
 
     // banner-button scrolls to another sections
 
-    $('.banner__btn').click(function() {
-        $('body').animate({
-            scrollTop: $('.about').offset().top
-        }, 1000)
-    });
+    // $('.banner__btn').on('click', function() {
+    //     $('main').animate({
+    //         scrollTop: $('.about').offset().top
+    //     }, 1000)
+
+    // });
 
 
+    // team slider
+
+    let slideIndex = 0;
+    startSlide();
+
+
+
+    function startSlide(n) {
+
+        const slide = document.querySelectorAll('.team-box');
+        const dots = document.querySelectorAll('.dots__item');
+
+        //     //hide slides
+        for (let i = 0; i < slide.length; i++) {
+            slide[i].style.display = 'none';
+        }
+
+        // remove active claas from dots
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(' active', '');
+        }
+
+
+        slideIndex++;
+        if (slideIndex > slide.length) {
+            slideIndex = 1;
+        }
+
+        //     //show slides
+        slide[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].className += ' active';
+        setTimeout(startSlide, 8000);
+    }
 
 
 })
